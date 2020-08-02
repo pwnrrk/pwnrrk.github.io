2e2c449f27acd7f8fa9a0af9c2f304ab6d1d7fbd
R.Phuwanat <ggamegg170@gmail.com>
2020-08-03 00:54:23 +0700

    upadte layout and fix navbar

--- app/app.js
+++ app/app.js
@@ -1,28 +1,27 @@
 function autoScroll(view) {
     var top = document.getElementById(view).offsetTop;
-    window.scrollTo({top:top-40,behavior:'smooth'});
+    window.scrollTo({ top: top - 40, behavior: 'smooth' });
 }
-function backToTop(){
-    window.scrollTo({top:0,behavior:'smooth'});
+function backToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-var abt = document.getElementById('abt').offsetTop-50;
-var skl = document.getElementById('skl').offsetTop-50;
-var prt = document.getElementById('prt').offsetTop-50;
+var abt = document.getElementById('abt').offsetTop - 50;
+var skl = document.getElementById('skl').offsetTop - 50;
+var prt = document.getElementById('prt').offsetTop - 50;
 var hm = document.getElementById('home').offsetTop;
 document.addEventListener('scroll', function () {
     var els = this.querySelectorAll('.mnt-active');
     [].forEach.call(els, function (el) {
-        if (skl < window.scrollY) {
+        if (prt < window.scrollY) {
+            el.classList.remove('mnt-active');
+            document.getElementById('prmnt').classList.add('mnt-active');
+        } else if (skl < window.scrollY) {
             el.classList.remove('mnt-active');
             document.getElementById('skmnt').classList.add('mnt-active');
         } else if (abt < window.scrollY) {
             el.classList.remove('mnt-active');
             document.getElementById('abmnt').classList.add('mnt-active');
-        }else if (prt < window.scrollY) {
-            el.classList.remove('mnt-active');
-            document.getElementById('prmnt').classList.add('mnt-active');
-        }
-        else {
+        } else {
             el.classList.remove('mnt-active');
             document.getElementById('hmnt').classList.add('mnt-active');
         }
