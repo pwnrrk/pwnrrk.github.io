a50e0ec613938b409e471dd63f56428453efa9c5
R.Phuwanat <r.phuwanat.dev@gmail.com>
2022-01-13 13:32:03 +0700

    Updates

---
+++ precache-manifest.cce9c5a6837ea8760c8af381ce84f69a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a31ecdc3676b5bf29dc76df93e9ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b74a202df91cea91ad53",
+    "url": "/static/css/main.252e775e.chunk.css"
+  },
+  {
+    "revision": "0c209941452ad5195caf",
+    "url": "/static/js/2.bd2461a7.chunk.js"
+  },
+  {
+    "revision": "8d465fe03e60d5431f589b9022199703",
+    "url": "/static/js/2.bd2461a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f552d60846cec339074c",
+    "url": "/static/js/3.3bedd33e.chunk.js"
+  },
+  {
+    "revision": "b74a202df91cea91ad53",
+    "url": "/static/js/main.a48e1fc7.chunk.js"
+  },
+  {
+    "revision": "b97b07356235ae172c7f",
+    "url": "/static/js/runtime-main.a6aeea5e.js"
+  }
+]);
